3857180ba89238df163f077b3b012c927ef7752a
AdithyaSean <adithyasean@gmail.com>
2024-06-30 16:32:30 +0530

    modified:   datasets/singlish/reddit.jsonl 	new file:   src/non_ascii_detector.py 	modified:   src/remove_columns.py 	modified:   src/transliteration.py

--- src/remove_columns.py
+++ src/remove_columns.py
@@ -1,29 +1,14 @@
 import json
 
-def remove_url_column(input_file, output_file):
-    """Removes the 'url' column from a JSONL file.
+input_file_path = 'datasets/singlish/' + input('enter the file name: ') + '.jsonl'
+output_file_path = 'datasets/singlish/' + input('enter the output file name: ') + '.jsonl'
+column_name = input('enter the column to remove: ')
 
-    Args:
-        input_file (str): Path to the input JSONL file.
-        output_file (str): Path to save the modified JSONL file.
-    """
+def remove_columns(input_file_path, output_file_path, column_name):
+    with open(input_file_path, 'r', encoding='utf-8') as input_file, open(output_file_path, 'w', encoding='utf-8') as output_file:
+        for line in input_file:
+            json_obj = json.loads(line)
+            json_obj.pop(column_name, None)
+            output_file.write(json.dumps(json_obj, ensure_ascii=False) + '\n')
 
-    with open(input_file, 'r', encoding='utf-8') as infile, \
-            open(output_file, 'w', encoding='utf-8') as outfile:
-        
-        for line in infile:
-            data = json.loads(line)
-            del data['english_question']
-            del data['english_answer']
-            del data['url']
-            json.dump(data, outfile, ensure_ascii=False)
-            outfile.write('\n')  # Add newline after each JSON object
-
-
-# Get input and output file paths from the user
-input_file = input("Enter the path to the JSONL file: ")
-output_file = input("Enter the desired path for the new JSONL file: ")
-
-remove_url_column(input_file, output_file)
-
-print(f"The 'url' column has been removed. New file saved to: {output_file}")
+remove_columns(input_file_path, output_file_path, column_name)

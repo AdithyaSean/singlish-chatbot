d62a22684bb45390370e326f4fde790e48259109
AdithyaSean <adithyasean@gmail.com>
2024-07-04 14:22:12 +0530

    new file:   datasets/english/vertex-ai_model-evaluation_gemini_pro_peft_train_sample.jsonl 	modified:   datasets/singlish/reddit.jsonl 	deleted:    src/parquet_to_jsonl.py 	new file:   src/process_jsonl.py 	deleted:    src/remove_columns.py 	deleted:    src/remove_text_patterns.py 	deleted:    src/vertex_ai_notebook.ipynb

--- src/parquet_to_jsonl.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-parquet_file = input("Enter the path to the parquet file: ")
-
-jsonl_file = input("Enter the path to the output JSONL file: ")
-
-df = pd.read_parquet(parquet_file)
-
-df.to_json(jsonl_file, orient='records', lines=True)
